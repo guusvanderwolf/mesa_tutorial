fb000c4307fb3ca21fcb8bdb0b2d6e078f13df47
Guus van der Wolf <g.v.d.wolf@student.tue.nl>
2025-12-09 22:31:25 +0100

    Finished creating your first model tutorial

--- money_model.py
+++ money_model.py
@@ -9,3 +9,57 @@ import pandas as pd
 import seaborn as sns
 
 import mesa
+
+import matplotlib.pyplot as plt
+
+class MoneyAgent(mesa.Agent):
+    """An agent with fixed initial wealth."""
+
+    def __init__(self, model):
+        # Pass the parameters to the parent class.
+        super().__init__(model)
+
+        # Create the agent's variable and set the initial values.
+        self.wealth = 1
+
+    def exchange(self):
+        # Verify agent has some wealth
+        if self.wealth > 0:
+            other_agent = self.random.choice(self.model.agents)
+            if other_agent is not None:
+                other_agent.wealth += 1
+                self.wealth -= 1
+
+
+class MoneyModel(mesa.Model):
+    """A model with some number of agents."""
+
+    def __init__(self, n=10):
+        super().__init__()
+        self.num_agents = n
+
+        # Create agents
+        MoneyAgent.create_agents(model=self, n=n)
+
+    def step(self):
+        """Advance the model by one step."""
+        # This function psuedo-randomly reorders the list of agent objects and
+        # then iterates through calling the function passed in as the parameter
+        self.agents.shuffle_do("exchange")
+
+all_wealth = []
+# This runs the model 100 times, each model executing 30 steps.
+for _ in range(100):
+    # Run the model
+    model = MoneyModel(10)
+    for _ in range(100):
+        model.step()
+
+    # Store the results
+    for agent in model.agents:
+        all_wealth.append(agent.wealth)
+
+# Use seaborn
+g = sns.histplot(all_wealth, discrete=True)
+g.set(title="Wealth distribution", xlabel="Wealth", ylabel="number of agents")
+plt.show()
