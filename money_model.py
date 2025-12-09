2950e5df3b95467f487265b6510e133cbc5a6b29
Guus van der Wolf <g.v.d.wolf@student.tue.nl>
2025-12-09 18:08:46 +0100

    Installed mesa package

---
+++ money_model.py
@@ -0,0 +1,11 @@
+# Has multi-dimensional arrays and matrices.
+# Has a large collection of mathematical functions to operate on these arrays.
+import numpy as np
+
+# Data manipulation and analysis.
+import pandas as pd
+
+# Data visualization tools.
+import seaborn as sns
+
+import mesa
